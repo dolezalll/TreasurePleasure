c61205c6269ead84d9a1afbf398283840db4a029
dolezalll <clamwinantivirus@gmail.com>
2023-04-28 22:43:47 +0200

    zmena databaze .sql

--- app/databaze.sql
+++ app/databaze.sql
@@ -1,8 +1,99 @@
-CREATE DATABASE myDB;
-USE myDB;
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Počítač: 127.0.0.1
+-- Vytvořeno: Čtv 27. dub 2023, 21:00
+-- Verze serveru: 10.4.24-MariaDB
+-- Verze PHP: 8.1.6
 
-CREATE TABLE users (
-  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Databáze: `ukol`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `games`
+--
+
+CREATE TABLE `games` (
+  `games_id` int(11) NOT NULL,
+  `pocet_tahu` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `velikost_pole_width` int(11) DEFAULT NULL,
+  `velikost_pole_height` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Vypisuji data pro tabulku `games`
+--
+
+INSERT INTO `games` (`games_id`, `pocet_tahu`, `user_id`, `velikost_pole_width`, `velikost_pole_height`) VALUES
+(1, 2, 1, 4, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(6) UNSIGNED NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Vypisuji data pro tabulku `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`) VALUES
+(1, 'vanecekdavid5@gmail.com', 'Kokot13');
+
+--
+-- Indexy pro exportované tabulky
+--
+
+--
+-- Indexy pro tabulku `games`
+--
+ALTER TABLE `games`
+  ADD PRIMARY KEY (`games_id`);
+
+--
+-- Indexy pro tabulku `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pro tabulky
+--
+
+--
+-- AUTO_INCREMENT pro tabulku `games`
+--
+ALTER TABLE `games`
+  MODIFY `games_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pro tabulku `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
